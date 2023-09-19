3e93b252bfb894aec51a435220efd1095b074be8
daytona65 <niclim65@gmail.com>
2023-09-18 21:58:00 -0400

    Loading page complete and responsive

--- src/components/sections/Biography/biography.jsx
+++ src/components/sections/Biography/biography.jsx
@@ -9,7 +9,7 @@ function Counter({ n }) {
   const { number } = useSpring({
     from: { number: 0 },
     number: n,
-    delay: 200,
+    delay: 4000,
     config: { mass: 1, tension: 20, friction: 10},
   });
   return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>
